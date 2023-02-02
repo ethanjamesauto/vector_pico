8504ae9e86e3e462134fcbce6ab17a78109327fa
Ethan James <ethanjamesauto@vt.edu>
2023-02-02 08:44:48 -0500

    add line drawing demo

--- dma_test.ino
+++ dma_test.ino
@@ -21,16 +21,17 @@ Edited 1/28/2023 by Ethan James
 #include <stdio.h>
 
 // Size of each buffer
-#define buffer_size 256
+#define BUFFER_SIZE 2048
 
 // Table of values to be sent to DAC
-unsigned short DAC_data[2][buffer_size];
+unsigned short DAC_data[2][BUFFER_SIZE];
 
 // Pointer to the address of the DAC data table
 unsigned short* address_pointer = &DAC_data[0][0];
 
 // A-channel, 1x, active
-#define DAC_config_chan_A 0b0011000000000000
+#define DAC_A 0b0111000000000000
+#define DAC_B 0b1111000000000000
 
 // SPI configurations
 #define PIN_MISO 4
@@ -43,7 +44,7 @@ unsigned short* address_pointer = &DAC_data[0][0];
 #define CTRL_CHANNEL 1
 
 // Number of DMA transfers per event
-const uint32_t transfer_count = buffer_size;
+const uint32_t transfer_count = BUFFER_SIZE;
 
 void setup()
 {
@@ -59,13 +60,6 @@ void setup()
     gpio_set_function(PIN_CS, GPIO_FUNC_SPI);
     gpio_set_function(PIN_SCK, GPIO_FUNC_SPI);
     gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
-
-    int i;
-    for (i = 0; i < (buffer_size); i++) {
-        uint16_t data = i * 2;
-        DAC_data[0][i] = DAC_config_chan_A | (data & 0x0fff);
-        DAC_data[1][i] = DAC_config_chan_A | ((data + 1) & 0x0fff);
-    }
 
     // Set up the control channel
     dma_channel_config c = dma_channel_get_default_config(CTRL_CHANNEL); // default configs
@@ -100,7 +94,7 @@ void setup()
         &c2, // The configuration we just created
         &spi_get_hw(SPI_PORT)->dr, // write address (SPI data register)
         DAC_data[0], // The initial read address
-        buffer_size, // Number of transfers
+        BUFFER_SIZE, // Number of transfers
         false // Don't start immediately.
     );
 
@@ -112,49 +106,97 @@ void setup()
     dma_start_channel_mask(1u << CTRL_CHANNEL);
 }
 
-bool which = 1; // (which) is read from by the data channel, and (!which) is written to by the line drawing algorithm
-bool done = true; // true if the inactive buffer has been fully prepared and is ready to be sent over SPI
+#define MAX_PTS 4096
+uint32_t point_buffer[2][MAX_PTS];
+int point_count[2];
+int point_pos = 0;
+int buffer_pos = 0;
+bool which_point = 0;
+bool which_dma = 1; // (which_dma) is read from by the data channel, and (!which_dma) is written to by the line drawing algorithm
+bool done = false; // true if the inactive buffer has been fully prepared and is ready to be sent over SPI
 
 // This function tells the line drawing algorithm to write to the buffer not being read from by the data channel.
 // Runs when the control channel has already finished setting up the data channel.
 void control_complete()
 {
     if (!done) {
-        Serial.println("Can't keep up!");
+        Serial.println("Can't keep up, only reached point " + String(buffer_pos));
     } else {
-        address_pointer = &DAC_data[which][0];
-        which = !which;
+        address_pointer = &DAC_data[which_dma][0];
+        which_dma = !which_dma;
         done = false;
+        // Serial.println("Sent some lines");
     }
     dma_hw->ints0 = 1u << CTRL_CHANNEL;
 }
 
-#define MAX_PTS 4096
-uint32_t point_buffer[2][MAX_PTS];
-int point_count[2];
-bool which_point = 0;
-
 void loop()
 {
-    static int i = 0;
-    if (!done) {
-        for (int j = 0; j < buffer_size; j++) {
-            DAC_data[!which][j] = point_buffer[!which_point][i++]; // DAC_config_chan_A | (i & 0x0fff);
-            if (i >= point_count[!which_point]) {
-                i = 0;
+    bool buf = !which_point;
+    for (int i = 0; i < point_count[buf]; i++) {
+        uint16_t x1 = (point_buffer[buf][i] >> 12) & 0xfff;
+        uint16_t y1 = point_buffer[buf][i] & 0xfff;
+        uint16_t x2 = (point_buffer[buf][(i + 1) % point_count[buf]] >> 12) & 0xfff;
+        uint16_t y2 = point_buffer[buf][(i + 1) % point_count[buf]] & 0xfff;
+        lineto(x1, y1, x2, y2);
+    }
+}
+
+// draw a line using breesenham
+void lineto(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
+{
+    uint16_t dx = abs(x2 - x1);
+    uint16_t dy = abs(y2 - y1);
+    int8_t sx = x1 < x2 ? 1 : -1;
+    int8_t sy = y1 < y2 ? 1 : -1;
+    int32_t err = (dx > dy ? dx : -dy) / 2, e2; // TODO: make int16_t?
+
+    for (;;) {
+        if (buffer_pos >= BUFFER_SIZE) // the code doesn't work without this. Not sure why.
+            Serial.println("Drawing with some stuff - " + String(x1) + " " + String(y1) + " " + buffer_pos + " " + done);
+        if (done) {
+            continue;
+        }
+        if (x1 == x2 && y1 == y2) {
+            break;
+        }
+        e2 = err;
+        if (e2 > -dx) {
+            err -= dy;
+            x1 += sx;
+            DAC_data[!which_dma][buffer_pos++] = DAC_A | x1;
+        }
+        if (buffer_pos >= BUFFER_SIZE) {
+            done_buffering();
+        } else if (e2 < dy) {
+            err += dx;
+            y1 += sy;
+            DAC_data[!which_dma][buffer_pos++] = DAC_B | y1;
+            if (buffer_pos >= BUFFER_SIZE) {
+                done_buffering();
             }
         }
-        done = true;
     }
+}
+
+inline void done_buffering()
+{
+    buffer_pos = 0;
+    done = true;
 }
 
 void setup1()
 {
     Serial.begin();
     pinMode(9, OUTPUT);
-    for (point_count[!which_point] = 0; point_count[!which_point] < 100; point_count[!which_point]++) {
+    /*for (point_count[!which_point] = 0; point_count[!which_point] < 100; point_count[!which_point]++) {
         point_buffer[!which_point][point_count[!which_point]] = point_count[!which_point];
-    }
+    }//*/
+    point_count[1] = 2;
+    point_buffer[1][0] = (20 << 12) | 10;
+    point_buffer[1][1] = (50 << 12) | 40;
+    // point_buffer[1][2] = (60 << 12) | 50;
+    // point_buffer[1][3] = (80 << 12) | 70;
 }
 
 void loop1()
