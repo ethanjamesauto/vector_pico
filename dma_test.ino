83aa3efac6607c57c748b2ac17b7b087a25b0324
Ethan James <ethanjamesauto@vt.edu>
2023-03-06 19:00:29 -0500

    mostly working with serial

--- dma_test.ino
+++ dma_test.ino
@@ -29,7 +29,7 @@ uint16_t line_speed = 3;
 uint16_t jump_speed = 150;
 
 uint32_t point_buffer[2][MAX_PTS];
-int point_count[2];
+uint16_t point_count[2];
 int point_pos = 0;
 bool which_point = 0;
 
@@ -47,79 +47,115 @@ void setup()
 void loop()
 {
 }
-/*
-// draw a line using breesenham
-void lineto(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t step)
-{
-    x1 /= step;
-    y1 /= step;
-    x2 /= step;
-    y2 /= step;
 
-    uint16_t dx = abs(x2 - x1);
-    uint16_t dy = abs(y2 - y1);
-    int8_t sx = x1 < x2 ? 1 : -1;
-    int8_t sy = y1 < y2 ? 1 : -1;
-    int32_t err = (dx > dy ? dx : -dy) / 2, e2; // TODO: make int16_t?
-
-    for (;;) {
-        if (x1 == x2 && y1 == y2) {
-            break;
-        }
-        e2 = err;
-        if (e2 > -dx) {
-            err -= dy;
-            x1 += sx;
-            DAC_data[!which_dma][buffer_pos++] = DAC_X | (x1 * step);
-            if (buffer_pos >= BUFFER_SIZE) {
-                done_buffering();
-            }
-        }
-
-        if (e2 < dy) {
-            err += dx;
-            y1 += sy;
-            DAC_data[!which_dma][buffer_pos++] = DAC_Y | (y1 * step);
-            if (buffer_pos >= BUFFER_SIZE) {
-                done_buffering();
-            }
-        }
-    }
-}
-*/
-// This interrupt service routine tells the line drawing algorithm to write to the buffer not being read from by the data channel.
-// Runs when the control channel has already finished setting up the data channel.
-
-enum vector_sm_state { LINE_0,
-    LINE_1,
-    LINE_2
+enum vector_sm_state { START,
+    LINE_10,
+    LINE_11,
+    LINE_X,
+    LINE_Y,
+    LINE_DONE,
+    JUMP
 };
 
-void vector_sm_execute(int i)
+#define JUMP_SPEED 8
+#define DRAW_SPEED 3
+
+inline void vector_sm_execute(int i) __attribute__((always_inline));
+inline void vector_sm_execute(int i)
 {
     static uint16_t x = 0;
     static uint16_t y = 0;
-    //static bool dir = 1;
-    static int n = 0;
-    static vector_sm_state state = LINE_0;
-#define DAC_MAX 4095
+    static vector_sm_state state = START;
+    static uint16_t point = 0;
+    static uint16_t jump_ctr;
+    static uint8_t brightness;
 
-    switch (state) {
-    case LINE_0:
-        state = LINE_1;
-        break;
-    case LINE_1:
-        DAC_data[which_dma][i] = (i % 2) ? (DAC_X | n++) : (DAC_Y | (DAC_MAX - n++));
-        if (n == DAC_MAX) {
-            state = LINE_2;
+    // breesenham variables
+    static int32_t dx, dy, sx, sy, err, e2; // TODO: maybe change the types
+    static uint16_t x1, y1;
+    for (int k = 0; k < 1000; k++) {
+        switch (state) {
+        case START:
+            if (point_count[POINT_READ] == 0) {
+                state = LINE_DONE;
+                break;
+            }
+            brightness = (point_buffer[POINT_READ][point] >> 24) & 0x3f;
+            x1 = ((point_buffer[POINT_READ][point] >> 12) & 0xfff);
+            y1 = (point_buffer[POINT_READ][point] & 0xfff);
+            if (brightness == 0 || point == 0) {
+                jump_ctr = ((abs(x1 - x) + abs(y1 - y)) >> JUMP_SPEED) + 2;
+                state = JUMP;
+                break;
+            }
+            x1 >>= DRAW_SPEED;
+            y1 >>= DRAW_SPEED;
+            x >>= DRAW_SPEED;
+            y >>= DRAW_SPEED;
+            dx = abs(x1 - x);
+            dy = abs(y1 - y);
+            sx = x < x1 ? 1 : -1;
+            sy = y < y1 ? 1 : -1;
+            err = (dx > dy ? dx : -dy) / 2;
+            state = LINE_10;
+            break;
+        case JUMP:
+            if (jump_ctr <= 0) {
+                x = x1;
+                y = y1;
+                point++;
+                if (point >= point_count[POINT_READ]) {
+                    point = 0;
+                }
+                state = START;
+                break;
+            }
+            jump_ctr--;
+            if (jump_ctr % 2 == 0) {
+                DAC_data[which_dma][i] = DAC_X | x1;
+            } else {
+                DAC_data[which_dma][i] = DAC_Y | y1;
+            }
+            return;
+        case LINE_X:
+            break;
+        case LINE_Y:
+            break;
+        case LINE_10:
+            if (x == x1 && y == y1) {
+                state = LINE_DONE;
+                break;
+            }
+            e2 = err;
+            if (e2 > -dx) {
+                err -= dy;
+                x += sx;
+                DAC_data[which_dma][i] = DAC_X | (x << DRAW_SPEED);
+                state = LINE_11;
+                return;
+            }
+            state = LINE_11;
+            break;
+        case LINE_11:
+            if (e2 < dy) {
+                err += dx;
+                y += sy;
+                DAC_data[which_dma][i] = DAC_Y | (y << DRAW_SPEED);
+                state = LINE_10;
+                return;
+            }
+            state = LINE_10;
+            break;
+        case LINE_DONE:
+            x = (point_buffer[POINT_READ][point] >> 12) & 0xfff;
+            y = point_buffer[POINT_READ][point] & 0xfff;
+            point++;
+            if (point >= point_count[POINT_READ]) {
+                point = 0;
+            }
+            state = START;
+            break;
         }
-        break;
-    case LINE_2:
-        DAC_data[which_dma][i] = (i % 2) ? (DAC_X | n--) : (DAC_Y | (DAC_MAX - n--));
-        if (n == 0) {
-            state = LINE_1;
-        }
-        break;
     }
 }
 
@@ -144,13 +180,13 @@ void setup1()
 {
     Serial.begin();
     pinMode(SERIAL_LED, OUTPUT);
-    point_count[1] = 5;
-    point_count[0] = 0;
-    point_buffer[1][0] = (0 << 24) | (0 << 12) | 0;
-    point_buffer[1][1] = (1 << 24) | (4095 << 12) | 0;
-    point_buffer[1][2] = (1 << 24) | (4095 << 12) | 4095;
-    point_buffer[1][3] = (1 << 24) | (0 << 12) | 4095;
-    point_buffer[1][4] = (1 << 24) | (0 << 12) | 0;
+    point_count[POINT_READ] = 5;
+    point_count[POINT_WRITE] = 1;
+    point_buffer[POINT_READ][0] = (0 << 24) | (0 << 12) | 0;
+    point_buffer[POINT_READ][1] = (1 << 24) | (4095 << 12) | 0;
+    point_buffer[POINT_READ][2] = (1 << 24) | (4095 << 12) | 4095;
+    point_buffer[POINT_READ][3] = (1 << 24) | (0 << 12) | 4095;
+    point_buffer[POINT_READ][4] = (1 << 24) | (0 << 12) | 0;
 }
 
 void loop1()
@@ -160,35 +196,38 @@ void loop1()
     static bool waiting = true;
 
     // read from Serial
-    if (!frame_ready)
-        while (Serial.available() > 0) {
-            digitalWriteFast(SERIAL_LED, HIGH);
-            char b = Serial.read();
-            if (waiting && b != 0) { // if we receive a nonzero byte, we're ready to start the next frame
-                waiting = false;
-                // frame_ready = 0;
-                point_count[which_point] = 0;
-            }
-            if (!waiting) {
-                cmd = (cmd << 8) | b; // build up a 32-bit command
-                if (++cmd_count == 4) { // see if we have a full command
-                    cmd_count = 0; // reset the command counter
-                    if (cmd == 0x01010101) { // if we receive a "done" command
-                        frame_ready = 1;
-                        waiting = true; // go back to waiting
-                    } else if (point_count[which_point] < MAX_PTS) {
-                        point_buffer[which_point][point_count[which_point]++] = cmd; // add the point to the buffer, then increment the point count
-                    }
-                    if (cmd == 0) {
-                        /*
-                        Somehow we reached the gap between one frame and another without recieving a done command.
-                        This can occur if the pico starts recieving data mid-frame and is unable to sync. If this happens, just go back to waiting for the next frame.
-                        */
-                        waiting = true;
-                    }
+    // if (!frame_ready) {
+    if (Serial.available() > 0) {
+        digitalWriteFast(SERIAL_LED, HIGH);
+        char b = Serial.read();
+        if (waiting && b != 0) { // if we receive a nonzero byte, we're ready to start the next frame
+            waiting = false;
+            // frame_ready = 0;
+            point_count[POINT_WRITE] = 0;
+        }
+        if (!waiting) {
+            cmd = (cmd << 8) | b; // build up a 32-bit command
+            if (++cmd_count == 4) { // see if we have a full command
+                cmd_count = 0; // reset the command counter
+                if (cmd == 0x01010101) { // if we receive a "done" command
+                    // frame_ready = true;
+                    if (point_count[POINT_WRITE] > 0)
+                        which_point = !which_point;
+                    waiting = true; // go back to waiting
+                } else if (point_count[POINT_WRITE] < MAX_PTS) {
+                    point_buffer[POINT_WRITE][point_count[POINT_WRITE]++] = cmd; // add the point to the buffer, then increment the point count
+                }
+                if (cmd == 0) {
+                    // Somehow we reached the gap between one frame and another without recieving a done command.
+                    // This can occur if the pico starts recieving data mid-frame and is unable to sync. If this happens, just go back to waiting for the next frame.
+                    waiting = true;
                 }
             }
-            digitalWriteFast(SERIAL_LED, LOW);
         }
-    delayMicroseconds(200);
+        digitalWriteFast(SERIAL_LED, LOW);
+    }
+    /*} else {
+        char b = Serial.read();
+        //delayMicroseconds(100);
+    }*/
 }
