5716f869d5ee3febf9a9f06a00ed524ea2f2e58d
Ethan James <ethanjamesauto@vt.edu>
2023-03-18 22:05:45 -0400

    changed comments

--- vector-pico.c
+++ vector-pico.c
@@ -27,15 +27,16 @@ int main()
     begin_frame();
     //*
     draw_moveto(-neg, -neg);
-    /*draw_to_xyrgb(pos, -neg, 255, 255, 255);
+    /*
+    draw_to_xyrgb(pos, -neg, 255, 255, 255);
     draw_to_xyrgb(pos, pos, 255, 255, 255);
     draw_to_xyrgb(-neg, pos, 255, 255, 255);
     draw_to_xyrgb(-neg, -neg, 255, 255, 255);
     draw_string("VSTCM-Pico", -1800, -100, 20, 255);
-    draw_string("2023 Ethan James", -1800, -500, 15, 255);*/
+    draw_string("2023 Ethan James", -1800, -500, 15, 255);
+    //*/
     draw_test_pattern();
     draw_moveto(-neg, -neg);
-    //*/
     // show_vstcm_splash_screen();
     multicore_launch_core1(draw_loop);
     end_frame();
