1e3c349da5fad70c8c1132bd95f4a21f9bac0ff5
Ethan <ethanjamesauto@gmail.com>
2024-01-14 14:36:32 -0500

    Move some stuff around

--- src/drawing_hw.c
+++ src/drawing_hw.c
@@ -53,12 +53,13 @@ void init()
     gpio_put(BLANK_PIN, 0);
 
     // set up SPI state machines
-    uint offset = pio_add_program(PIO, &spi_cpha0_cs_program);
 #ifdef MCP4922
+    uint offset = pio_add_program(PIO, &spi_cpha0_cs_program);
     for (int i = 5; i <= 7; i++)
         gpio_set_function(i, GPIO_FUNC_PIO0);
     pio_spi_cs_init(PIO, SM_X, offset, 16, 133e6 / (20e6 * 2), 5, 7);
 #else
+    uint offset = pio_add_program(PIO, &spi_cpha0_cs_program);
     for (int i = 5; i <= 10; i++)
         gpio_set_function(i, GPIO_FUNC_PIO0);
     pio_spi_cs_init(PIO, SM_X, offset, 24, 133e6 / (24e6 * 2), 5, 7);
